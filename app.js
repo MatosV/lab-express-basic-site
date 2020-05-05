04c1aa8dd85c5ce8dcb72d95a9e4240c41424c3f
Vasco Matos <vascomato@gmail.com>
2020-05-05 21:26:14 +0100

    few changes on the css, but im getting some bug trying to figure...

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 //html
 app.get('/home', (request, response) => {
